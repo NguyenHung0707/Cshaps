5f774fbc4cfe09ab27e8f54d2b154f3e8ebb41b5
NguyenHung0707 <111960391+NguyenHung0707@users.noreply.github.com>
2023-05-15 11:29:50 +0700

    C#

---
+++ Atom.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+class Atomic
+{
+    private string number;
+    private string symbol;
+    private string fullname;
+    private string weight;
+
+    public Atomic(string number, string symbol, string fullname, string weight)
+    {
+        this.number = number;
+        this.symbol = symbol;
+        this.fullname = fullname;
+        this.weight = weight;
+    }
+
+    public Atomic()
+    {
+    }
+
+    ArrayList array = new ArrayList();
+
+     public Boolean accept()
+    {
+        System.Console.WriteLine("Enter atomic number : ");
+        number = Console.ReadLine();
+
+        System.Console.WriteLine("Enter symbol : ");
+        symbol = Console.ReadLine();
+
+        System.Console.WriteLine("Enter full name : ");
+        fullname = Console.ReadLine();
+
+        System.Console.WriteLine("Enter automic weight : ");
+        weight = Console.ReadLine();
+
+        Atomic at = new Atomic(number, symbol, fullname, weight);
+        array.Add(at);
+        return true;
+    }
+
+    public void Display()
+    {
+        foreach(Atomic at in array)
+        {
+            System.Console.WriteLine(at.number + " " + at.symbol + " " + at.fullname + " " + at.weight );
+        }
+    }
+}
---
+++ testAtom.cs
@@ -0,0 +1,16 @@
+﻿class test
+{
+    public static void Main(string[] args)
+    {
+        Atomic at = new Atomic();
+        int i = 0;
+        do
+        {
+            i++;
+            at.accept();
+
+        } while (i != 3);
+        System.Console.WriteLine("------------------------------------");
+        at.Display();
+    }
+}
